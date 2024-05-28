076626b1dad342f9d5e6f963e74acf3af27e7a6a
jenea130 <jenea130@mail.ru>
2024-05-29 00:03:26 +0300

    all files theme bs-vite

---
+++ front-page.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Template Name: Front page
+ */
+get_header();
+?>
+<h2><?php echo get_the_title(); ?></h2>
+<ul class="some">
+  <li><a href="#">Link 1</a></li>
+  <li><a href="#">Link 2</a></li>
+  <li><a href="#">Link 3</a></li>
+  <li><a href="#">Link 4</a></li>
+</ul>
+<?php get_footer(); ?>
